46193ea0f165bb59ed0a41a2843776abf05ff799
wldlsgur <87266785+wldlsgur@users.noreply.github.com>
2022-10-26 22:07:24 +0900

    리액트 기본 셋팅

--- react_todolist/src/App.js
+++ react_todolist/src/App.js
@@ -1,8 +1,19 @@
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/main" component={} />
+          <Route path="/profile" component={} />
+          <Route path="/" component={} />
+          <Route render={() => <div className="error">에러 페이지</div>} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
