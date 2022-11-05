b0ede25c9eea39276614005435ce0e724d9258c5
wldlsgur <dhgg321@naver.com>
2022-11-05 20:12:16 +0900

    리덕스 따로 빼서 공부

--- react_todolist/src/App.js
+++ react_todolist/src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/HOME/home";
-import store from "./stre/Store/store.js";
 
 const store = createStore(CountReducer);
 
