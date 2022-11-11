1fbc4f3c15d42372230dd23b2640b208a9fe3573
wldlsgur <dhgg321@naver.com>
2022-11-11 15:34:19 +0900

    그냥 커밋

--- react_todolist/src/App.js
+++ react_todolist/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/HOME/home";
 
-const store = createStore(CountReducer);
+// const store = createStore(CountReducer);
 
 function App() {
   return (
@@ -10,6 +10,15 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} exact />
+          <Route
+            path="/hi"
+            element={
+              <div>
+                <h4>hi</h4>
+              </div>
+            }
+            exact
+          />
         </Routes>
       </BrowserRouter>
     </div>
